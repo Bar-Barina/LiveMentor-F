830680bb76e5a928ce9330c798462e704abbdfeb
Bar-Barina <itsbarbarina@gmail.com>
2023-08-01 01:11:26 +0300

    added mobile for edit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,8 +5,8 @@ export function Loader() {
     return (
         <section className='loader'>
             <ThreeCircles
-                height="100"
-                width="100"
+                height="70"
+                width="70"
                 color="#222222"
                 wrapperStyle={{}}
                 wrapperClass=""
