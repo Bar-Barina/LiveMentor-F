126bbe0a4f093364e11dd4d3b57d242a0b5d5e62
Bar-Barina <itsbarbarina@gmail.com>
2023-07-31 22:33:44 +0300

    added crud actions

--- src/store/actions/codeblock.actions.js
+++ src/store/actions/codeblock.actions.js
@@ -1,5 +1,5 @@
 import { codeblockService } from "../../services/codeblock.service";
-import { SET_CODEBLOCKS } from "../reducers/codeblock.reducer";
+import { SET_CODEBLOCKS, REMOVE_CODEBLOCK, ADD_CODEBLOCK, UPDATE_CODEBLOCK, SET_FILTER_BY } from "../reducers/codeblock.reducer";
 
 export function loadCodeblocks() {
   return async (dispatch) => {
@@ -14,5 +14,42 @@ export function loadCodeblocks() {
     } catch (error) {
       console.log("error:", error);
     }
+  };
+}
+
+export function remove(codeblockId) {
+  return async (dispatch) => {
+    try {
+      await codeblockService.remove(codeblockId);
+      const action = { type: REMOVE_CODEBLOCK, codeblockId };
+      dispatch(action);
+      // showSuccessMsg(`Removed successfully`);
+      return "Removed!";
+    } catch (error) {
+      // showErrorMsg(`Something went wrong`);
+      console.log("error:", error);
+    }
+  };
+}
+
+export function save(codeblock) {
+  return async (dispatch) => {
+    try {
+      const type = codeblock._id ? UPDATE_CODEBLOCK : ADD_CODEBLOCK;
+      const newCodeblock = await codeblockService.save(codeblock);
+      const action = { type, codeblock: newCodeblock };
+      dispatch(action);
+      // showSuccessMsg(`codeblock ${type}ed successfully`);
+      return "Removed!";
+    } catch (error) {
+      // showErrorMsg(`Something went wrong`);
+      console.log("error:", error);
+    }
+  };
+}
+
+export function setFilterBy(filterBy) {
+  return (dispatch) => {
+    dispatch({ type: SET_FILTER_BY, filterBy });
   };
 }
