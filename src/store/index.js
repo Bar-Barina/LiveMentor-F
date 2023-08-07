10671c7bdf8de9cd693ecdf721aad1c16ba09670
Bar-Barina <itsbarbarina@gmail.com>
2023-08-07 13:42:56 +0300

    test

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,7 @@ import { codeblockReducer } from "./reducers/codeblock.reducer";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-  codeblockReducer, 
+  codeblockReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
